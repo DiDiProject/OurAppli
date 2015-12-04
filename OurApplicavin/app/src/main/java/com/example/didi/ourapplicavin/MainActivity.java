8b8710c84afb5f9eae395fdcf1c16a8aaccba683
DiDiProject <delphdu78@gmail.com>
2015-12-04 12:26:21 +0100

    ajout de deux packages (controleursIHM et modèle) ajout de la classe AffichageMenuPrincipal (pas ajout dedans)
    changement dans la classe principal (MainActivity) et son layout:
    -layout: ajout bouton (menu principal), photo vins (donc ajout dans le drawable de la photo photovin.jpg (remarque ne pas mettre de majuscule pour le nol d'une photo))
    -classe: bouton->change activité (AffichageMenuPrincipal)
    
    commit 2
    didi

--- OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/MainActivity.java
+++ OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/MainActivity.java
@@ -1,37 +1,40 @@
 package com.example.didi.ourapplicavin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.didi.ourapplicavin.controleursIHM.AffichageMenuPrincipal;
 
+//classe principale, celle qui va se lancer lors de l'ouverture de l'application
+//juste un écran d'accueil avec un bouton pour ensuite accéder au menu principal
+public class MainActivity extends Activity {
+    //attribut
+    private Button menuPrincipal = null;
+
+    //méthode qui se fait en premier quand on lanc l'application
+    //car nous sommes dans la classe principale
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_main); //on affiche l'écran d'accueil
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //on va chercher le bouton pour accéder ensuite au menu principal
+        menuPrincipal = (Button)findViewById(R.id.allerMenuPrincipal);
+        //ainsi lorsqu'on appuie sur ce bouton il va dans une autre activité (le menu principale de l'application))
+        menuPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pour dire à l'utilisateur que le menu principal va s'afficher (texte qui s'affiche pas longtemps en bas)
+                Toast.makeText(MainActivity.this, "Le contenu du menu principal va s'afficher !", Toast.LENGTH_SHORT).show();
+                //on va chercher l'activité
+                Intent n = new Intent(MainActivity.this, AffichageMenuPrincipal.class);
+                startActivity(n); //on commence une nouvelle activité
+            }
+        });
     }
 }
