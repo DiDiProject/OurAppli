d4facbb8df74985770821202426f88302d9ece60
DiDiProject <delphdu78@gmail.com>
2015-12-06 15:22:56 +0100

    commit 25  (didi) Vin et ListeVin
    -Vin (attributs avec leur getter, constructeurs)
    -ListeVin (attributs avec leur getter, constructeurs et méthodes (ajout vin, suppr vin)
    
    => pas fini

--- OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/modeles/ListeVin.java
+++ OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/modeles/ListeVin.java
@@ -1,7 +1,68 @@
 package com.example.didi.ourapplicavin.modeles;
 
+import java.util.ArrayList;
+
 /**
+ * Classe d'une liste de vins
  * Created by didi on 05/12/2015.
  */
 public class ListeVin {
+    //Attributs
+    private ArrayList<Vin> listeVins; //liste de vins
+    private int nombreVins; //nombre de vin dans la liste
+
+    //Constructeur d'initialisation d'une liste de vins (vide)
+    public void ListeVin (){
+        this.listeVins = new ArrayList<>();
+        this.nombreVins = 0;
+    }
+
+    //Constructeur d'initialisation d'une liste de vins en ayant déjà une liste
+    public void ListeVin(ArrayList<Vin> listeVin){
+        this.listeVins = listeVin;
+        this.nombreVins = listeVin.size();
+    }
+
+    //Méthode pour avoir la liste des vins
+    public ArrayList<Vin> getListeVins() {
+        return listeVins;
+    }
+
+    //Méthode pour avoir un vin en ayant le nom du vin
+    public Vin getVin(String nom){
+        Vin vin = new Vin();
+        //on parcourt la liste
+        for(int i =0; i< listeVins.size(); i++){
+            //si le nom du vin est le même, on retourne ce vin
+            if(nom == listeVins.get(i).getNom()){
+                vin = listeVins.get(i);
+                break ;
+            }
+        }
+        return vin;
+    }
+
+    //Méthode pour ajouter un vin dans la liste
+    public void addVin(Vin vin){
+        listeVins.add(nombreVins +1,vin); //on ajoute ce vin dans la liste
+        nombreVins +=1; //le nombre de vin augemente de 1
+    }
+
+    //Méthode pour supprimer un vin de la liste
+    public void supprVin(Vin vin){
+        //on parcourt la liste des vins
+        for(int i=0; i<listeVins.size(); i++){
+            //si le vin est le même que celui à supprimer on l'enlève dans la liste
+            if(vin == listeVins.get(i)){
+                listeVins.remove(i);
+                nombreVins -=1; //le nombre de vin diminue de 1
+                return ;
+            }
+        }
+    }
+
+    //Méthode pour avoir le nombre de vins dans la liste
+    public int getNombreVins() {
+        return nombreVins;
+    }
 }
--- OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/modeles/Vin.java
+++ OurApplicavin/app/src/main/java/com/example/didi/ourapplicavin/modeles/Vin.java
@@ -1,7 +1,51 @@
 package com.example.didi.ourapplicavin.modeles;
 
 /**
+ * //Classe Object vin
  * Created by didi on 05/12/2015.
  */
 public class Vin {
+    //Attributs
+    private String nom; //nom du vin
+    private String couleur; //couleur du vin
+    private String cepage; //cépage du vin
+    private String region; //région du vin
+
+    //Constructeur d'inialisation vin (vide)
+    public void Vin(){
+        // TODO
+    }
+
+    //Constructeur d'inialisation vin (avec le nom)
+    public void Vin(String nom) {
+        this.nom = nom;
+    }
+
+    //Constructeur d'inialiation d'un vin (avec le nom, la couleur, le cépage et la région du vin)
+    public Vin(String nom, String couleur, String cepage, String region) {
+        this.nom = nom;
+        this.couleur = couleur;
+        this.cepage = cepage;
+        this.region = region;
+    }
+
+    //Méthode pour avoir le nom du vin
+    public String getNom() {
+        return nom;
+    }
+
+    //Méthode pour avoir la couleur du vin
+    public String getCouleur() {
+        return couleur;
+    }
+
+    //Méthode pour avoir le cépage du vin
+    public String getCepage() {
+        return cepage;
+    }
+
+    //Méthode pour avoir la région du vin
+    public String getRegion() {
+        return region;
+    }
 }
